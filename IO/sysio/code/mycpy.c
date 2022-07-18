3e635b2b9d823890ddca17751d41b73de4112374
mmjwxbc <2891002167@qq.com>
2022-07-18 10:45:22 +0800

    Parallel1~5

--- IO/sysio/code/mycpy.c
+++ IO/sysio/code/mycpy.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 #define BUFSIZE 1048576
 
 int main(int argc, char **argv)
@@ -14,24 +15,40 @@ int main(int argc, char **argv)
         fprintf(stderr, "Usage...\n");
         exit(1);
     }
-    int fdr = open(argv[1], O_RDONLY);
-    if(fdr < 0)
+    int fdr;
+    do{
+        fdr = open(argv[1], O_RDONLY);
+        if(fdr < 0)
+        {
+            if(errno != EINTR)
+            {
+                perror("open()");
+                exit(1);
+            }
+        }
+    }while(fdr < 0);
+    int fdw;
+    do
     {
-        perror("open()");
-        exit(1);
-    }
-    int fdw = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if(fdw < 0)
-    {
-        perror("open()");
-        close(fdr);
-        exit(1);
-    }
+        fdw = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0600);
+        if(fdw < 0)
+        {
+            if(errno != EINTR)
+            {
+                perror("open()");
+                close(fdr);
+                exit(1);
+            }
+        }        
+    } while (fdw < 0);
+
     while(1)
     {
         int len = read(fdr, buf, BUFSIZE);
         if(len < 0)
         {
+            if(errno == EINTR)
+                continue;
             perror("read()");
             break;
         }
@@ -43,6 +60,8 @@ int main(int argc, char **argv)
             int ret = write(fdw, buf + pos, len);
             if(ret < 0)
             {
+                if(errno == EINTR)
+                    continue;
                 perror("write()");
                 exit(1);
             }
